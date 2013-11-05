f5ee3b574fb60722001787dead9c6d21febe6e4e
Aurélien Thévoz <aurelienthevoz@gmail.com>
2013-11-05 13:44:42 +0100

    Fix findRulesByType namedquery

--- src/main/java/ch/heigvd/skeleton/model/Rule.java
+++ src/main/java/ch/heigvd/skeleton/model/Rule.java
@@ -25,7 +25,7 @@ import javax.persistence.NamedQuery;
     ),
     @NamedQuery(
         name = "findRulesByType",
-        query = "SELEC r FROM Rule where r.onEventType = @onEventType"
+        query = "SELEC r FROM Rule where r.onEventType = :onEventType"
     )
 })
 

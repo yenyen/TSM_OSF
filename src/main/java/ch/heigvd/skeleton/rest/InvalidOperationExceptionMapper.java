0e12ea5c106b6a0f5498fccbcc924393cded689e
Aurélien Thévoz <aurelienthevoz@gmail.com>
2013-11-11 09:54:31 +0100

    Add throws InvalidOperationException and fix duplicate apiKey check

--- src/main/java/ch/heigvd/skeleton/rest/InvalidOperationExceptionMapper.java
+++ src/main/java/ch/heigvd/skeleton/rest/InvalidOperationExceptionMapper.java
@@ -5,6 +5,7 @@
  */
 
 package ch.heigvd.skeleton.rest;
+import ch.heigvd.skeleton.exceptions.InvalidOperationException;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
